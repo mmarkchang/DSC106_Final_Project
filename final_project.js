4b393849144098c3bf177a7cfa688952c5009410
mmarkchang <49168436+mmarkchang@users.noreply.github.com>
2020-03-20 03:32:09 -0700

    progress

--- final_project.js
+++ final_project.js
@@ -1,8 +1,12 @@
 function init() {
     pieChart();
-    barChart2();
+    lineChart();
     pieChart2();
     slider();
+    top100Promise.then(function (top100) {
+        barChart(top100, 2017);
+        
+    })
 }
 
 
@@ -82,27 +86,83 @@ function barChart(json, year) {
     });
 }
 
-function barChart2() {
-	var dingus = 2;
-	var widget = 4;
-    
-    Highcharts.chart('bar-chart2', {
-        chart: {
-            type: 'pie'
-        },
+function updateLine() {
+
+}
+
+function lineChart(player) {
+    var points_dict = {
+        "Novak Djokovic" : [20, 229, 515, 1465, 4470, 5390, 10650, 8310, 14720, 13630, 13280, 13130, 16785, 16950, 11780],
+        "Andy Murray" :  [37, 65, 587, 1400, 2125, 3720, 9700, 7800, 7825, 8570, 9360, 5560, 8945, 12685, 12560],
+        "Kei Nishikori" : [0, 0, 37, 150, 652, 1325, 554, 1500, 2315, 2495, 5025, 6385, 4905, 5010],
+        "Alexander Zverev" : [0, 0, 0, 0, 0, 0, 0, 0, 15, 31, 398, 689, 1745, 4610],
+        "Milos Raonic" : [0, 0, 0, 2, 12, 106, 345, 1367, 2380, 2860, 4440, 5160, 5450, 5450]
+    }
+    var rank_dict = {
+        "Novak Djokovic" : [690, 410, 122, 44, 6, 3, 3, 2, 2, 1, 1, 1, 1, 1, 4],
+        "Andy Murray" :  [656, 516, 247, 35, 13, 9, 3, 4, 4, 4, 3, 8, 3, 2, 2],
+        "Kei Nishikori" : [0, 0, 955, 424, 117, 178, 323, 50, 18, 15, 11, 6, 6, 10],
+        "Alexander Zverev" : [0, 0, 0, 0, 0, 0, 0, 0, 1153, 803, 437, 99, 39, 13],
+        "Milos Raonic" : [0, 0, 0, 1389, 1031, 645, 279, 41, 20, 13, 9, 9, 8, 9]
+    }
+    console.log(player);
+    Highcharts.chart('line-chart', {
+
         title: {
-            text: 'Total Dingus vs. Widget'
+            text: 'Solar Employment Growth by Sector, 2010-2016'
         },
+    
+        subtitle: {
+            text: 'Source: thesolarfoundation.com'
+        },
+    
+        yAxis: {
+            title: {
+                text: 'Number of Employees'
+            }
+        },
+    
+        xAxis: {
+            accessibility: {
+                rangeDescription: 'Range: 2010 to 2017'
+            }
+        },
+    
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+    
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 2003
+            }
+        },
+    
         series: [{
-            data: [{
-                name: 'Dingus',
-                y: dingus
-            },
-            {
-                name: 'Widget',
-                y: widget
+            name: 'Installation',
+            data: points_dict[player]
+        }],
+    
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
             }]
-        }]
+        }
+    
     });
 }
 
